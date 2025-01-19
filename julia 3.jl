d6bf1dd508583f16f5a289741efd2892696b4aef
sergioura <sergio.ura@gmail.com>
2025-01-19 03:42:38 -0300

    Add files via upload

---
+++ julia 3.jl
@@ -0,0 +1,44 @@
+"""
+################################################################
+#### caution: slow, very time consuming
+#### calculate dendograms
+############################################################
+"""
+
+
+using PyCall
+
+os = pyimport("os")
+np = pyimport("numpy")
+@pyimport pandas as pd
+shutil = pyimport("shutil")
+@pyimport persim
+
+
+
+####################################
+## empty the indicated folder
+function clear_folder(folder)
+    # folder = '/path/to/folder'
+    for filename in os.listdir(folder)
+        file_path = os.path.join(folder, filename)
+        try
+            rm(file_path, force=true, recursive=true) #rm = remove
+        catch e #Exception as e
+            @printf("Failed to delete %s. Reason: %s", file_path, e)
+        end
+    end
+end
+#######################################################################
+
+function mkfolder(newpath)
+    if os.path.exists(newpath)
+        println("Folder $newpath already exists")
+        clear_folder(newpath)
+    else
+        os.mkdir(newpath)# make directory
+        println("Folder $newpath created!")
+    end
+end
+
+
