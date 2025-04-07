a17e07d533592c0dbe6e538032322c07bf93c4d9
vituri <vituri.vituri@gmail.com>
2025-04-07 00:36:41 -0300

    add blur to preprocessing

--- src/preprocessing/functions.jl
+++ src/preprocessing/functions.jl
@@ -1,20 +1,17 @@
-using Images
+using Images, ImageFiltering
 
-function image_to_grey(path)        
-    img_gray = load(path) .|> Gray    
-
-    img_gray
+function image_to_grey(path; blur = 1)        
+    imfilter(load(path), Kernel.gaussian(blur)) .|> Gray
 end
 
-function image_to_r2(image::AbstractString, threshold = 0.5)
-    image = image_to_grey(image)
-    image_to_r2(image, threshold)    
+function image_to_r2(image::AbstractString; threshold = 0.5, blur = 1)
+    image = image_to_grey(image, blur = blur)
+    image_to_r2(image, threshold = threshold)
 end
 
-function image_to_r2(image::Matrix, threshold = 0.5)    
+function image_to_r2(image::Matrix; threshold = 0.5)
     m = convert(Array{Float32}, image)
     pts = [[i[1], i[2]] for i ∈ findall(<(0.5), m)]
 
     pts
 end
-
--- using/preprocessing.jl
+++ using/preprocessing.jl
@@ -1,8 +1,12 @@
-using TDAfly
 using TDAfly.Preprocessing
 
+# reading image and converting to points in R^2
 path = "images/processed/Asilidae 11.png"
-img_gray = image_to_grey(path)
+img_gray = image_to_grey(path, blur = 1)
+
 pts = image_to_r2(img_gray)
+
+# no need to read image in other step
+path = "images/processed/Asilidae 11.png"
 pts = image_to_r2(path)
 
